5d211b3b42a2f71366b059ed14427cf5d1e3fd34
Nico513c <61504641+Nico513c@users.noreply.github.com>
2020-04-20 09:45:19 +0200

    Add files via upload

--- JS.js
+++ JS.js
@@ -1,1 +1,6 @@
-
+besparelse(){
+a = parseFloat(prompt('Hvor mange måneder har du brugt Microsoft Office eller en anden kontorpakke som koster penge'))
+b = parseFloat(prompt('Hvor meget har du betalt om måneden for den kontorpakke?'))
+c = a * b
+alert('Summen af det beløb du kunne have sparet ved at bruge LibreOffice er ' + c)
+}
